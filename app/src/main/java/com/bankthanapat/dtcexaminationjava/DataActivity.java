ecd4f0b8819b0fb7f62dbed633efdca7eea32f44
thanapat munmanothum <bankthanapat@gmail.com>
2023-12-23 17:28:38 +0700

    fix little bug

--- app/src/main/java/com/bankthanapat/dtcexaminationjava/DataActivity.java
+++ app/src/main/java/com/bankthanapat/dtcexaminationjava/DataActivity.java
@@ -1,9 +1,14 @@
 package com.bankthanapat.dtcexaminationjava;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Adapter;
 import android.widget.AdapterView;
@@ -24,6 +29,11 @@ public class DataActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("หน้าแสดงข้อมูล");
+
         listView = (ListView) findViewById(R.id.animalList);
 
         mHelper = new DBHelper(this);
@@ -31,7 +41,7 @@ public class DataActivity extends AppCompatActivity {
         String[] animals = animalList.toArray(new String[animalList.size()]);
         Arrays.sort(animals);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, animals);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.custom_list_item, animals);
         listView.setAdapter(adapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -43,5 +53,39 @@ public class DataActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_search, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+
+                return true;
+            }
+        });
+
+        return true;
     }
 }
